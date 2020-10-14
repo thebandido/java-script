f1b0db51d363a57387d36981925cc6b92bd3aabf
thebandido <rokcet6@gmail.com>
2020-10-14 15:36:57 +0300

    update

---
+++ Middle course/main.js
@@ -0,0 +1,11 @@
+import { auth } from "./recovery.js";
+
+const a = 10;
+
+console.log(10 * a);
+
+// const recoveryJs = require(`./recovery.js`);
+
+// console.log(recoveryJs);
+
+auth();
---
+++ Middle course/recovery.js
@@ -0,0 +1,43 @@
+export const auth = () => {
+  console.log(`hello new user`);
+};
+
+// export const logOut = () => {
+//   `GOOD BYE USER :()`;
+// };
+
+// class Car {
+//   constructor(speed, color = `white`, price = 1000) {
+//     this.speed = speed || 0;
+//     this.color = color;
+//     this.price = price;
+//     // this.disstance = disstance(speed);
+//     this.engineIsStarted = false;
+//     this.startTime = this.timerOn();
+//   }
+
+//   turnOn() {
+//     if (!this.engineIsStarted) {
+//       this.engineIsStarted = true;
+//       this.startTime;
+//     }
+//   }
+
+//   timerOn() {
+//     return Date.now();
+//   }
+
+//   disstance(speed) {
+//     return speed * this.drivetime;
+//   }
+// }
+
+// const car = new Car(null);
+
+// console.log(car);
+
+// const message = `hello world`;
+
+// module.exports = {
+//   message,
+// };
